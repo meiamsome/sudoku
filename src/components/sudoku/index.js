e2b561d7a9803aa68cb2ad3960b17927480c6aa5
Oliver Wright <meiamsome@meiamso.me>
2017-05-10 19:14:12 +0100

    First part of Sudoku component

--- src/components/sudoku/index.js
+++ src/components/sudoku/index.js
@@ -3,12 +3,32 @@ import React from 'react';
 export default class Sudoku extends React.Component {
   constructor(...props) {
     super(...props);
-    this.state = {};
+    this.state = {
+      grid: [...Array(9).keys()].map(_ => [...Array(9).keys()].map(i => i)),
+    };
   }
 
   render() {
     return (
-      <div>Test</div>
-    )
+      <table>
+        <tbody>
+          {
+            this.state.grid.map((row, rindex) => {
+              return (
+                <tr key={rindex}>
+                  {
+                    row.map((cell, cindex) => {
+                      return (
+                        <td key={cindex}>{cell}</td>
+                      )
+                    })
+                  }
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    );
   }
 };
