a3e377ef5e7f506e45c0b6a316431f03d8d44694
Oliver Wright <meiamsome@meiamso.me>
2017-05-26 23:10:58 +0100

    Add link to today's sudoku on homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Col, Grid } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
+    var now = new Date();
+    var month = now.getUTCMonth() + 1;
+    if(month < 10) month = "0" + month;
+    var day = now.getUTCDate();
+    if(day < 10) day = "0" + day;
+    var today = now.getUTCFullYear() + "-" + month + "-" + day;
     return (
       <div>
         <h1>Sudoku</h1>
         <p>Welcome to the sudoku website.</p>
+        <Link to={"/sudoku/" + today + "/"}>Today&rsquo;s Sudoku</Link>
       </div>
     );
   }
